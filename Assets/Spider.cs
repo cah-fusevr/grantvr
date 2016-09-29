0547c2bb230bf76279b961adb4b2ad3ce278895d
Grant James <grant.james@cardinalhealth.com>
2016-09-29 16:57:47 -0400

    spidernit stage

---
+++ Assets/Spider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animation>().CrossFade ("attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetComponent<Animation>().atta
+
+	}
+}
